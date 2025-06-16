09dd4bbc295232c4d40891bd3c5a098bef8532a6
phoenix-110-135 <a87h97j2022@gmail.com>
2025-06-16 16:32:31 +0330

    get event of mouse in js

--- humburger-Menu/script.js
+++ humburger-Menu/script.js
@@ -1,3 +1,13 @@
 function toggleMenu() {
     document.body.classList.toggle('menu-open');
   }
+  
+const cursor = document.getElementById('circle');
+let mouseX = 0, mouseY = 0;
+let currentX = 0, currentY = 0;
+
+document.addEventListener("mousemove", (e) => {
+mouseX = e.clientX;
+mouseY = e.clientY;
+});
+  
