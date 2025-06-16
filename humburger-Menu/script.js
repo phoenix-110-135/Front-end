e54a99433cadd4586862d40e55013dde95c035bb
phoenix-110-135 <a87h97j2022@gmail.com>
2025-06-16 16:34:32 +0330

    add animate function and import animate func

--- humburger-Menu/script.js
+++ humburger-Menu/script.js
@@ -1,7 +1,7 @@
 function toggleMenu() {
     document.body.classList.toggle('menu-open');
   }
-  
+
 const cursor = document.getElementById('circle');
 let mouseX = 0, mouseY = 0;
 let currentX = 0, currentY = 0;
@@ -11,3 +11,11 @@ mouseX = e.clientX;
 mouseY = e.clientY;
 });
   
+function animate() {
+    currentX += (mouseX - currentX) * 0.15;
+    currentY += (mouseY - currentY) * 0.15;
+    cursor.style.transform = `translate(${currentX}px, ${currentY}px) translate(-50%, -50%)`;
+    requestAnimationFrame(animate);
+}
+
+animate();
