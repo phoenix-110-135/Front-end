3173ff3a8fa59b918e7c0afd2354b9c60489315d
phoenix-110-135 <a87h97j2022@gmail.com>
2025-06-16 11:30:00 +0330

    Update script.js

--- product hero section/script.js
+++ product hero section/script.js
@@ -1,4 +1,4 @@
-let listclass =  [
+let listClass = [
     {
         current : 'img-1',
         next : 'img-3',
@@ -10,7 +10,7 @@ let listclass =  [
         next : 'img-1',
         bg : '#b5b4af',
     }
-    ,
+    , 
     {
         current : 'img-3',
         next : 'img-2',
@@ -18,13 +18,14 @@ let listclass =  [
     }
 ]
 
-const handleChangeslide = ()=> {
-    let images = document.querySelectorAll('img-slide')
+
+const handleChangeSlide = ()=> {
+    let images = document.querySelectorAll('.img-slide')
     for(let img of images){
-        for(let j in listclass){
-            if(img.classList.contains(listclass[j])){
-                img.classList.replace(listclass[j].current,listclass[j].next);
-                document.getElementById('circle-bg').style.backgroundColor = listclass[j].bg;
+        for(let j in listClass){
+            if(img.classList.contains(listClass[j].current)){
+                img.classList.replace(listClass[j].current,listClass[j].next);
+                document.getElementById('circle-bg').style.backgroundColor = listClass[j].bg;
                 document.getElementById('circle-bg').style.transition = '1.5s';
                 break
             }
@@ -33,5 +34,5 @@ const handleChangeslide = ()=> {
 }
 
 setInterval(()=>{
-    handleChangeslide();
+    handleChangeSlide();
 },2000)
